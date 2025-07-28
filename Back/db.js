ec05c0fb70cd2e47727d5bbab72b7f61cbf62659
SCatanoC <santiago47c@gmail.com>
2025-07-28 11:54:00 -0500

    variables

--- Back/db.js
+++ Back/db.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 
 // conexión
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
 });
 
 //conect
