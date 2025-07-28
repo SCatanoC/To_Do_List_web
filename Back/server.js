a8b9c221f14357c91c10b368987c8a2e76391c25
SCatanoC <santiago47c@gmail.com>
2025-07-28 11:48:36 -0500

    server

--- Back/server.js
+++ Back/server.js
@@ -1,15 +1,28 @@
+// backend/server.js
 const express = require("express");
 const cors = require("cors");
-const taskRoutes = require("./routes/tasks");
+const taskroutes = require("./routes/tasks");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000; // <-- importante para Render
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/tasks", taskRoutes);
+// API routes
+app.use("/api/tasks", taskroutes);
 
+// Servir archivos estáticos desde "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta para servir index.html en la raíz
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
